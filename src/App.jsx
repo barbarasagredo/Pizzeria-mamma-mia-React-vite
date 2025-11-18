74a0af77388b64bf50b74a95422ffec42d7c5525
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-11-17 23:58:32 -0300

    se crea el componente Profile

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Login from "./pages/Login";
 import { Pizza } from "./pages/Pizza";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
 
 const App = () => {
   // const pizzaList = pizzas;
@@ -26,7 +27,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/pizza/p001" element={<Pizza />} />
-        {/* <Route path="/profile" element={<Profile />}></Route> */}
+        <Route path="/profile" element={<Profile />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
       {/* <Login />
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  const email = "usuario@profile.cl";
+  return (
+    <>
+      <div className="container-fluid col-8">
+        <div className="d-flex flex-column align-items-center border border-light-subtle shadow-sm rounded gap-3 p-5">
+          <div className="border rounded-circle border-2 border-bottom-0 profile-img-container border-light-subtle shadow-sm">
+            <i class="fa-solid fa-user text-secondary fs-2"></i>
+          </div>
+          <span className="text-secondary ">{email}</span>
+          <button type="button" class="btn btn-dark">
+            Cerrar sesión
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
