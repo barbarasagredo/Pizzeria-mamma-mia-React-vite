cf145f70734a2ae7837b29f05f570db3c65d241a
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-11-30 18:44:53 -0300

    Se corrigen errores de escritura de código

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="//login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/pizza/p001" element={<Pizza />} />
