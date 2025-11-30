dc6cca78b1b3878d35d746a2775912c3a3131a62
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-11-30 18:40:30 -0300

    Se reemplaza react-router-dom por react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 
 import App from "./App.jsx";
 
